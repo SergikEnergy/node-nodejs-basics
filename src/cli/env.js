2450997f88e0b8949d198c9be882d84dbb94b7ab
skustov <skustov@clevertec.ru>
2024-10-03 00:15:47 +0300

    feat(*): implement cli module

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
+import { env } from 'node:process';
+
 const parseEnv = () => {
-    // Write your code here 
+  const envPairs = Object.entries(env);
+  const res = [];
+
+  envPairs.forEach(([key, value]) => {
+    if (key && key.match('RSS_')) {
+      res.push(`${key}=${value}`);
+    }
+  });
+
+  console.log(res.join('; '));
 };
 
 parseEnv();
+
+/* env.js - implement function that parses environment variables with prefix RSS_ and prints them to the console in the format RSS_name1=value1; RSS_name2=value2 */
